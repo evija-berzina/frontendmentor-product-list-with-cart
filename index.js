5e5e05be7bd9bf227df3e71cd6b2f48ca2664adc
eberzina <berzina3633@gmail.com>
2025-12-28 22:45:42 +0200

    Add cart rendering and product quantity handling

--- index.js
+++ index.js
@@ -1,36 +1,138 @@
+let products = [];
+
 fetch('/data.json')
     .then(res => res.json())
     .then(data => {
+      products = data;
+
+      products.forEach((product, index) => {
+        product.id = index + 1;
+        product.quantity = 0;
+      })
 
       const productCard = document.querySelector('.products-js');
-      let productsHTML = '';
+      const cart = document.querySelector('.cart-js');
 
-      data.forEach((product) => {
-        // const product = data[index];
-        productsHTML += `
-          <article class="product-card">
-            <div class="product-card__img-container flex">
-              <picture>
-                <source srcset="${product.image.desktop}" media="(min-width: 45rem)" />
-                <source srcset="${product.image.tablet}" media="(min-width: 35rem)" />
-                <img class="product-card__image" src="${product.image.mobile}" width="654" height="424" alt="${product.name}">
-              </picture>
-              <button class="product-card__btn flex product-btn-js">
-                <img src="assets/images/icon-add-to-cart.svg" alt="">
-                <span>Add to Cart</span>
-              </button>
-            </div>
-            <div class="product-card__content flex">
-              <p class="product-card__category">${product.category}</p>
-              <p class="product-card__name">${product.name}</p>
-              <p class="product-card__price">
-                <span>&#36</span>${(product.price).toFixed(2)}
-              </p>
-            </div>
-          </article>
-        `;
-      });
+      renderProducts(products);
+      renderCart(products);
 
-      productCard.innerHTML = productsHTML;
+      function renderProducts(products) {
+        let productsHTML = '';
+
+        products.forEach((product) => {
+          // const product = data[index];
+          productsHTML += `
+            <article class="product-card" data-id="${product.id}">
+              <div class="product-card__img-container flex">
+                <picture>
+                  <source srcset="${product.image.desktop}" media="(min-width: 45rem)" />
+                  <source srcset="${product.image.tablet}" media="(min-width: 35rem)" />
+                  <img class="product-card__image" src="${product.image.mobile}" width="654" height="424" alt="${product.name}">
+                </picture>
+                ${ product.quantity === 0
+                  ? `<button class="product-card__btn flex product-btn-js">
+                      <img src="assets/images/icon-add-to-cart.svg" alt="">
+                      <span>Add to Cart</span>
+                    </button>`
+                  : `<div class="quantity-controls flex">
+                      <button class="decrease-btn">
+                        <img src="assets/images/icon-decrement-quantity.svg" alt="">
+                      </button>
+                        <span>${product.quantity}</span>
+                      <button class="increase-btn">
+                        <img src="assets/images/icon-increment-quantity.svg" alt="">
+                      </button>
+                    </div>`
+                }
+              </div>
+              <div class="product-card__content flex">
+                <p class="product-card__category">${product.category}</p>
+                <p class="product-card__name">${product.name}</p>
+                <p class="product-card__price">
+                  <span>&#36</span>${(product.price).toFixed(2)}
+                </p>
+              </div>
+            </article>
+          `;
+        });
+
+        productCard.innerHTML = productsHTML;
+
+      }
+
+      productCard.addEventListener('click', (event) => {
+        const card = event.target.closest('.product-card');
+        if (!card) return;
+
+        const productId = Number(card.dataset.id);
+        const product = products.find(p => p.id === productId);
+
+        if (event.target.closest('.product-btn-js')) {
+          product.quantity = 1;
+        }
+
+        if (event.target.closest('.increase-btn')) {
+          product.quantity += 1;
+        }
+
+        if (event.target.closest('.decrease-btn')) {
+          product.quantity -= 1;
+          if (product.quantity < 0) product.quantity = 0;
+        }
+
+        renderProducts(products);
+        renderCart(products);
+        productCartInfo(products);
+    });
+
+        function renderCart(products) {
+          let cartHTML = '';
+
+          cartHTML += `
+            <article>
+              <h2 class="cart__title text-red js-cart-title">Your Cart (${totalQuantity(products)})</h2>
+                <img class="cart__img" src="assets/images/illustration-empty-cart.svg" alt="">
+                <div class="product-cart-info"></div>
+                <p class="cart__text">Your added items will appear here</p>
+                <p>Order total: $${totalOrderSum(products)}</p>
+            </article>
+          `;
+
+          cart.innerHTML = cartHTML;
           
+        }
+        
+        function totalQuantity(products) {
+          let sum = 0;
+            products.forEach(product => {
+              sum += product.quantity;
+            });
+          return sum;
+        }
+
+        function totalOrderSum(products) {
+          let sum = 0;
+            products.forEach(product => {
+              sum += (product.quantity * product.price);
+            });
+          return sum;
+        }
+
+        function productCartInfo(products) {
+          const cartProducts = document.querySelector('.product-cart-info');
+          let cartProductsHTML = '';
+          products.forEach(product => {
+            if (product.quantity > 0) {
+              cartProductsHTML += `
+                <p>${product.name}</p>
+                <p>${product.quantity}</p>
+                <p>${product.price}</p>
+                <p>${product.quantity * product.price}</p>
+              `;
+            } 
+          });
+
+            cartProducts.innerHTML = cartProductsHTML;
+        }
+      
 });
