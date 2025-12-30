b484e50dc01e189cb012976cceca9eb7fce3387c
eberzina <berzina3633@gmail.com>
2025-12-30 18:42:36 +0200

    Add and style cart section after product selection

--- index.js
+++ index.js
@@ -94,35 +94,46 @@ fetch('/data.json')
 
         function renderCart(products) {
           let cartHTML = '';
+          const product = totalQuantity(products);
 
           cartHTML += `
-            <article>
-              <h2 class="cart__title text-red js-cart-title">Your Cart (${totalQuantity(products)})</h2>
-                <img class="cart__img" src="assets/images/illustration-empty-cart.svg" alt="">
-                <div class="product-cart-info"></div>
-                <p class="cart__text">Your added items will appear here</p>
-                <p>Order total: $${totalOrderSum(products)}</p>
-            </article>
-          `;
-
+            ${ product.totalQuantity === 0
+              ? `<article class="product-cart-container flex">
+                <h2 class="cart__title text-red js-cart-title">Your Cart (${product.totalQuantity})</h2>
+                  <img class="cart__img" src="assets/images/illustration-empty-cart.svg" alt="">
+                  <div class="product-cart-info"></div>
+                  <p class="cart__text">Your added items will appear here</p>
+                </article>`
+              : `<article class="product-cart-container flex">
+              <h2 class="cart__title text-red js-cart-title">Your Cart (${product.totalQuantity})</h2>
+                  <div class="product-cart-info flex"></div>
+                  <div class="cart-total flex">
+                    <p class="cart-total__text">Order total:</p>
+                    <p class="cart-total__sum">$${product.totalOrderSum}</p>
+                  </div>
+                  <div class="cart__carbon-info flex">
+                    <svg class="carbon-icon" width="21" height="20" viewBox="0 0 21 20">
+                      <path d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z" />
+                      <path d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"/>
+                    </svg>
+                    <p class="carbon-info">This is a <span>carbon-neutral</span> delivery</p>
+                  </div>
+                  <button class="cart__confirm-btn flex">
+                  Confirm Order
+                  </button>
+              </article>`
+            }`
           cart.innerHTML = cartHTML;
-          
-        }
-        
-        function totalQuantity(products) {
-          let sum = 0;
-            products.forEach(product => {
-              sum += product.quantity;
-            });
-          return sum;
         }
 
-        function totalOrderSum(products) {
-          let sum = 0;
+        function totalQuantity(products) {
+          let totalQuantity = 0;
+          let totalOrderSum = 0;
             products.forEach(product => {
-              sum += (product.quantity * product.price);
+              totalQuantity += product.quantity;
+              totalOrderSum += (product.quantity * product.price);
             });
-          return sum;
+          return {totalQuantity, totalOrderSum};
         }
 
         function productCartInfo(products) {
@@ -131,10 +142,21 @@ fetch('/data.json')
           products.forEach(product => {
             if (product.quantity > 0) {
               cartProductsHTML += `
-                <p>${product.name}</p>
-                <p>${product.quantity}</p>
-                <p>${product.price}</p>
-                <p>${product.quantity * product.price}</p>
+              <article class="cart-product-container flex">
+                <div class="cart-text-container flex">
+                  <p class ="cart-product__name">${product.name}</p>
+                  <div class="cart-item-info flex">
+                    <p class="cart-product__quantity">${product.quantity}x</p>
+                    <p class="cart-product__price">@ $${product.price}</p>
+                    <p class="cart-product__total">$${product.quantity * product.price}</p>
+                  </div>
+                </div>
+                <button class="cart-remove-btn flex">
+                  <svg class="remove-icon" width="10" height="10"s viewBox="0 0 10 10">
+                    <path d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z" />
+                  </svg>
+                </button>
+              </article>
               `;
             } 
           });
