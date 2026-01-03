6cb62c47b21bfbb92a956f25e91706c4ca6b865d
eberzina <berzina3633@gmail.com>
2026-01-03 17:20:02 +0200

    Update styles and HTML; add media query

--- index.js
+++ index.js
@@ -35,7 +35,10 @@ fetch('./data.json')
                 </picture>
                 ${ product.quantity === 0
                   ? `<button class="product-card__btn flex product-btn-js">
-                      <img src="assets/images/icon-add-to-cart.svg" alt="">
+                      <svg class="cart-icon" width="18" height="17" viewBox="0 0 21 20">
+                        <path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/>
+                        <path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/>
+                      </svg>
                       <span>Add to Cart</span>
                     </button>`
                   : `<div class="quantity-controls flex">
@@ -110,7 +113,7 @@ fetch('./data.json')
                   <div class="product-cart-info flex"></div>
                   <div class="cart-total flex">
                     <p class="cart-total__text">Order total:</p>
-                    <p class="cart-total__sum">$${product.totalOrderSum}</p>
+                    <p class="cart-total__sum">$${product.totalOrderSum.toFixed(2)}</p>
                   </div>
                   <div class="cart__carbon-info flex">
                     <svg class="carbon-icon" width="21" height="20" viewBox="0 0 21 20">
@@ -148,8 +151,8 @@ fetch('./data.json')
                   <p class ="cart-product__name">${product.name}</p>
                   <div class="cart-item-info flex">
                     <p class="cart-product__quantity">${product.quantity}x</p>
-                    <p class="cart-product__price">@ $${product.price}</p>
-                    <p class="cart-product__total">$${product.quantity * product.price}</p>
+                    <p class="cart-product__price">@ $${product.price.toFixed(2)}</p>
+                    <p class="cart-product__total">$${(product.quantity * product.price).toFixed(2)}</p>
                   </div>
                 </div>
                 <button class="cart-remove-btn flex">
@@ -208,20 +211,20 @@ fetch('./data.json')
                   <img class="product-card__image" src="${p.image.thumbnail}" width="50" height="50" alt="${p.name}">
                   <div class="order-product-info flex">
                     <div class="cart-text-container flex">
-                      <p class ="cart-product__name">${p.name}</p>
+                      <p class ="cart-product__name truncate">${p.name}</p>
                     </div>
                     <div class="cart-item-info flex">
                       <p class="cart-product__quantity">${p.quantity}x</p>
-                      <p class="cart-product__price">@ $${p.price}</p>
+                      <p class="cart-product__price">@ $${p.price.toFixed(2)}</p>
                     </div>
                   </div>
-                  <p class="cart-product__total">$${p.quantity * p.price}</p>
+                  <p class="cart-product__total">$${(p.quantity * p.price).toFixed(2)}</p>
                 </div>
                 `).join("")
               }   
                 <div class="cart-total flex">
                   <p class="cart-total__text">Order total:</p>
-                  <p class="cart-total__sum">$${total.totalOrderSum}</p>
+                  <p class="cart-total__sum">$${total.totalOrderSum.toFixed(2)}</p>
                 </div>
               </div>
               <button class="new-order-btn">
@@ -238,7 +241,7 @@ fetch('./data.json')
             products.forEach(p => p.quantity = 0);
             renderProducts(products);
             renderCart(products);
+            order.innerHTML = "";
           }
-          order.innerHTML = "";
         });
 });
